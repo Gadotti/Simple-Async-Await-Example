6f5bac4533c2dbb528ae4caa759b72c88cae23f5
Eduardo F. Gadotti <gadotti.eduardo@gmail.com>
2017-07-15 12:34:45 +0100

    first commit

---
+++ Async-Await Example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async_Await_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting");
+
+            var worker = new Worker();
+            worker.DoWork();
+
+            while (!worker.IsComplete)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Async-Await Example/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async_Await_Example
+{
+    class Worker
+    {
+        public bool IsComplete { get; private set; }
+
+        public async void DoWork()
+        {
+            this.IsComplete = false;
+            Console.WriteLine("Doing work");
+
+            await LongOperation();
+
+            Console.WriteLine("Work completed");
+
+            IsComplete = true;
+        }
+
+        private Task LongOperation()
+        {
+            return Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Working!");
+                    Thread.Sleep(2000);
+                });
+        }
+    }
+}
